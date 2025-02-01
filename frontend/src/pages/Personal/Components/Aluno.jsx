5296ca0e6c6a5d50f6a32e1ad735359691bc6b17
ricardokks <161058381+ricardokks@users.noreply.github.com>
2025-02-01 16:12:37 -0300

    comço da criação da aba de alunos

---
+++ frontend/src/pages/Personal/Components/Aluno.jsx
@@ -0,0 +1,13 @@
+
+
+export function Aluno(){
+
+    return(
+          <div className="w-32 aspect-[9/16] glassBg border-2 border-zinc-500">
+
+
+
+          </div>
+
+    )
+}
--- frontend/src/pages/Personal/Section/alunos.jsx
+++ frontend/src/pages/Personal/Section/alunos.jsx
@@ -4,9 +4,12 @@ export function Alunos(){
 
 
     return(
-           <div className="w-full h-full">
-            <div className="w-full h-14">
-                <input type="text" placeholder="Pesquisar"/>
+           <div className="w-full h-full font-Outfit px-6">
+            <div className="w-full h-11 flex justify-end pr-6">
+                <input type="text" placeholder="Pesquisar..." className="w-1/3 min-w-64 glassBg h-full p-2 rounded-xl border-2 text-white placeholder-zinc-300  border-neutral-500 "/>
+
+            </div>
+            <div className="h-[calc(100%-44px)] w-full grid col-end-3 place-content-center place-items-center overflow-y-auto">
 
             </div>
 
