1693d820634678c510831ae668ea9f026981d37c
ricardokks <161058381+ricardokks@users.noreply.github.com>
2025-03-05 22:50:17 -0300

    Treinos filter

--- frontend/src/pages/Aluno/Section/treinos/criarTreinos.jsx
+++ frontend/src/pages/Aluno/Section/treinos/criarTreinos.jsx
@@ -3,6 +3,7 @@ import { TreinosCr } from "./components/treinoComple";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import api from '../../../../../@lib/api'
+import { set } from "date-fns";
 //import { CriarTreino } from "../Components/CriarTreino";
 
 export function CriarTreinos() {
@@ -12,6 +13,7 @@ export function CriarTreinos() {
   const [openEdit, setOpenEdit] = useState(0);
   const [treinos, setTreinos] = useState([]);
   const [openCria, setOpenCria] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
   //limite de carateres da descrição e 90
   useEffect(() => {
@@ -32,17 +34,22 @@ export function CriarTreinos() {
       });
   }, [treinos]);
 
+  const filteredTreinosNome = treinos.filter((treino) =>
+    treino.nome.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="w-full h-full font-Outfit px-6">
       <div className="w-full h-11 mt-4 flex-col flex justify-center items-end pr-6">
         <input
+        onChange={(e)=> setSearchTerm(e.target.value)}
           type="text"
           placeholder="Pesquisar..."
           className="w-1/3 max-2xl:w-1/2  min-w-64 glassBg h-full p-2 py-5 text-lg outline-1 outline-white/30 placeholder-white/50 rounded-xl border-3 text-white  border-neutral-500"
           />
       </div>
       <div className="h-[calc(100%-44px)] w-full grid grid-cols-3 gap-y-12 place-content-start place-items-center overflow-y-auto p-18 scroll-smooth">
-        {treinos.map((treino) => {
+        {filteredTreinosNome.map((treino) => {
           return (
             <Treino
               partesAfeto={treino.partesAfeto}
